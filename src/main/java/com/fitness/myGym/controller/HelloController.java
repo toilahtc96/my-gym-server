2832b47fa57b48ca7a228dc06931a233a31dfa77
toilahtc96 <toilahtc@gmail.com>
2024-04-24 21:50:39 +0700

    update code init

---
+++ src/main/java/com/fitness/myGym/controller/HelloController.java
@@ -0,0 +1,14 @@
+package com.fitness.myGym.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("hello")
+public class HelloController {
+    @GetMapping("/get")
+    public String hello() {
+        return "Hello";
+    }
+}
